9ca1166acce980fa79f7a6c531659aea247348d9
Jstallings8686 <85545463+Jstallings8686@users.noreply.github.com>
2023-04-02 17:47:03 -0500

    Add files via upload
    use this src file instead of the lone files

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Navbar from './components/Navbar';
+import ProductList from './components/ProductList';
+import Details from './components/Details';
+import Cart from './components/Cart';
+import Default from './components/Default';
+
+
+function App() {
+  return (
+    <React.Fragment>
+      <Navbar />
+      <Routes>
+        <Route exact path="/" element={<ProductList />} />
+        <Route path="/details" element={<Details />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="*" element={<Default />} />
+      </Routes>
+
+    </React.Fragment>
+  );
+}
+
+export default App;
